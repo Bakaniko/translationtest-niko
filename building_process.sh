3fe3fcd69c54058e98f02873ecef1a802c7ef596
Bakaniko <baka.niko@gmail.com>
2017-03-13 20:07:03 +0100

    Update building_process.sh

--- building_process.sh
+++ building_process.sh
@@ -1,2 +1,7 @@
+#! /bin/bash
+
+# clean the folder, make the .po files and send them to transifex
+# this is used to ease the testing process, it won't be in the final process
+
 rm -Rf build && make gettext && sphinx-intl update-txconfig-resources --pot-dir build/en/gettext/ --transifex-project-name test_osgeolive && tx push -s
 
